599a2d242af65604895eff42985bad8f4cd78464
Nelsonfrank <nelsonfr78@outlook.com>
2023-06-11 01:39:09 +0300

    feat(shared): Add profile item in header

--- src/components/button/icon-button/IconButton.tsx
+++ src/components/button/icon-button/IconButton.tsx
@@ -1,11 +1,25 @@
 import { IconType } from "react-icons";
-
+import { MdArrowDropDown } from "react-icons/md";
 export interface IconBtnPropType {
 	title?: string;
 	Icon: IconType;
 }
 
 export default function IconButton({ title, Icon }: IconBtnPropType) {
+	if (title?.toLowerCase() === "profile") {
+		return (
+			<>
+				<button className='flex flex-col items-center text-gray-500 px-4 pb-2 '>
+					<Icon className='text-3xl' />
+					<div className='flex items-center'>
+						<span className='capitalize text-sm'>Me</span>
+						<MdArrowDropDown />
+					</div>
+				</button>
+			</>
+		);
+	}
+
 	return (
 		<>
 			<button className='flex flex-col items-center text-gray-500 px-4 pb-2 border-b-2 border-transparent focus:text-black focus:border-black '>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -6,7 +6,7 @@ import { IoMdPeople } from "react-icons/io";
 import { FaBriefcase } from "react-icons/fa";
 import { BsChatDotsFill } from "react-icons/bs";
 import { MdNotifications } from "react-icons/md";
-
+import { CgProfile } from "react-icons/cg";
 
 export default function Header() {
 	return (
@@ -23,13 +23,14 @@ export default function Header() {
 							/>
 						</div>
 					</div>
-					<div className='w-75 self-end'>
-						<div className='flex gap-4'>
+					<div className='w-75'>
+						<div className='flex gap-4 items-center'>
 							<IconButton title='home' Icon={ImHome3} />
 							<IconButton title='My Network' Icon={IoMdPeople} />
 							<IconButton title='Jobs' Icon={FaBriefcase} />
 							<IconButton title='Messaging' Icon={BsChatDotsFill} />
 							<IconButton title='Notification' Icon={MdNotifications} />
+							<IconButton title='Profile' Icon={CgProfile} />
 						</div>
 					</div>
 				</div>
