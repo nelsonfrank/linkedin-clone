66ba1cb627def2cb989ca93138aeac7366a55fd9
Nelsonfrank <nelsonfr78@outlook.com>
2023-06-10 17:11:41 +0300

    feat: Setup firebase

---
+++ src/config/firebase.ts
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+	apiKey: "AIzaSyCnCA-pcT-1Ib_Rlw7VCY8av4nuaj6UKPo",
+	authDomain: "linkedin-clone-e6257.firebaseapp.com",
+	projectId: "linkedin-clone-e6257",
+	storageBucket: "linkedin-clone-e6257.appspot.com",
+	messagingSenderId: "538898708383",
+	appId: "1:538898708383:web:aa340580ec8f592e726faa",
+	measurementId: "G-2B9S0JH5T1",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
